e4b72eb41941b0431726cc732cb957572e255ac2
febiins <febinsunny2004@gmail.com>
2026-05-11 11:57:33 +0530

    Implement removeDuplicateLetters function

---
+++ RemoveDuplicateLetters.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+       
+        stack=[]
+
+        for i,ch in enumerate(s):
+            if ch in stack:
+                continue
+            while stack and ch < stack[-1] and stack[-1] in s[i+1:]:
+                stack.pop()
+            stack.append(ch)        
+                
+        return "".join(stack)       
+
